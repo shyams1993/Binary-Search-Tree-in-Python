8881e0481df3b81ab44ed56a3160468af85cba94
Shyam Salil <shyamsalil1993@gmail.com>
2020-06-14 22:18:19 +0530

    Update binarysearchtree.py

--- binarysearchtree.py
+++ binarysearchtree.py
@@ -1,8 +1,8 @@
 class Node():
     def __init__(self,value):
+        self.value = value
         self.left = None
         self.right = None
-        self.value = value
 
 class BinarySearchTree():
     def __init__(self):
@@ -10,7 +10,7 @@ class BinarySearchTree():
 
     def __str__(self):
         return str(self.__dict__)
-    
+
     def insert(self,value):
         newNode = Node(value)
         if self.root == None:
@@ -36,145 +36,75 @@ class BinarySearchTree():
         currentNode = self.root
         while True:
             if currentNode == None:
-                return "Not found"
-            elif currentNode.value == value:
-                return currentNode.value
-                break
+                return "Node doesn't exist"
             elif value < currentNode.value:
                 currentNode = currentNode.left
             elif value > currentNode.value:
                 currentNode = currentNode.right
+            elif value == currentNode.value:
+                return currentNode.value
+                break
 
     def remove(self,value):
-        parentNode = None
         currentNode = self.root
-        while True:
-            if value < currentNode.value and currentNode.left != None:
+        parentNode = None
+        while currentNode != None:
+            if value < currentNode.value:
                 parentNode = currentNode
                 currentNode = currentNode.left
-            elif value > currentNode.value and currentNode.right != None:
+            elif value > currentNode.value:
                 parentNode = currentNode
                 currentNode = currentNode.right
-            elif value == currentNode.value:
+            elif currentNode.value == value:
                 if currentNode.right == None:
-                    parentNode.left = currentNode.left
-                    currentNode = parentNode.left
-                    break
-                elif currentNode.left == None:
-                    parentNode.right = currentNode.right
-                    currentNode = parentNode.right
-                    break
-                elif currentNode.left == None and currentNode.right == None:
-                    if currentNode is parentNode.right:
-                        parentNode.right = None
-                        parentNode = currentNode
+                    if parentNode == None:
+                        self.root = currentNode.left
                         break
-                    elif currentNode is parentNode.left:
-                        parentNode.left = None
-                        parentNode = currentNode
-                        break
-                elif currentNode.right != None and currentNode.right.left == None:
-                    if currentNode is parentNode.left:
-                        if currentNode.right.right != None:
-                            parentNode.left = currentNode.right
-                            currentNode.right = currentNode.right.right
-                            parentNode.left.left = currentNode.left
-                            break
-                        if currentNode.right.right == None:
-                            parentNode.left = currentNode.right
-                            currentNode.right = None
-                            parentNode.left.right = None
-                            parentNode.left.left = currentNode.left
-                            break
-                    elif currentNode is parentNode.right:
-                        if currentNode.right.right != None:
-                            parentNode.right = currentNode.right
-                            currentNode.right = currentNode.right.right
-                            parentNode.right.left = currentNode.left
-                            break
-                        if currentNode.right.right == None:
-                            parentNode.right = currentNode.right
-                            currentNode.right = None
-                            parentNode.right.right = None
-                            parentNode.right.left = currentNode.left
-                            break
-                elif currentNode.right != None and currentNode.right.left != None:
-                    if currentNode is parentNode.left:
-                        if currentNode.right.right != None:
-                            parentNode.left = currentNode.right.left
-                            parentNode.left.right = currentNode.right
-                            currentNode.right = currentNode.right.right
-                            parentNode.left.left = currentNode.left
-                            currentNode.right.left = None
-                            break
-                        if currentNode.right.right == None:
-                            parentNode.left = currentNode.right.left
-                            parentNode.left.right = currentNode.right
-                            currentNode.right.right = None
-                            parentNode.left.left = currentNode.left
-                            currentNode.right.left = None
-                            break
-                    elif currentNode is parentNode.right:
-                        if currentNode.right.right != None:
-                            parentNode.right = currentNode.right.left
-                            parentNode.right.right = currentNode.right
-                            currentNode.right = currentNode.right.right
-                            parentNode.right.left = currentNode.left
-                            parentNode.right.right.left = None
-                            break
-                        if currentNode.right.right == None:
-                            parentNode.right = currentNode.right.left
-                            parentNode.right.right = currentNode.right
-                            currentNode.right.right = None
-                            parentNode.right.left = currentNode.left
-                            currentNode.right.left = None
-                            parentNode.right.right.left = None
-                            break                           
-                elif currentNode.right.right == None :
-                    if currentNode is parentNode.left:
-                        parentNode.left = currentNode.right
-                        currentNode.right = None
-                        if currentNode.left.left == None:
-                            parentNode.left.left = currentNode.left
-                        break
-                    elif currentNode is parentNode.right:
-                        parentNode.right = currentNode.right
-                        currentNode.right = None
-                        if currentNode.left.left == None:
-                            parentNode.right.left = currentNode.left
-                        break
-                elif currentNode.left.left == None :
-                    if currentNode is parentNode.left:
+                    elif currentNode.value < parentNode.value:
                         parentNode.left = currentNode.left
-                        currentNode.left = None
                         break
-                    elif currentNode is parentNode.right:
-                        parentNode.right = currentNode.right
-                        currentNode.right = None
+                    elif currentNode.value > parentNode.value:
+                        parentNode.right = currentNode.left
                         break
-                
+                elif currentNode.right.left == None:
+                    if parentNode == None:
+                        self.root = currentNode.right
+                        break
+                    elif currentNode.value < parentNode.value:
+                        parentNode.left = currentNode.right
+                        break
+                    elif currentNode.value > parentNode.value:
+                        parentNode.right == currentNode.right
+                        break
+                else:
+                    leftmost = currentNode.right.left
+                    while leftmost.left != None:
+                        leftmost = leftmost.left
+                    leftmost.left = currentNode.left
+                    leftmost.right = currentNode.right
+                    if parentNode == None:
+                        self.root = leftmost
+                        break
+                    else:
+                        if currentNode.value < parentNode.value:
+                            parentNode.left = leftmost
+                            break
+                        elif currentNode.value > parentNode.value:
+                            parentNode.right = leftmost
+                            break
+
+
 
 
 
 m = BinarySearchTree()
+m.insert(100)
 m.insert(50)
-m.insert(45)
 m.insert(40)
-m.insert(35)
-m.insert(30)
-m.insert(36)
-m.insert(37)
-m.insert(44)
-m.insert(42)
-m.insert(47)
-m.insert(46)
-m.insert(48)
-m.insert(65)
+m.insert(20)
+m.insert(75)
+m.insert(80)
 m.insert(60)
 m.insert(55)
-m.insert(62)
-m.insert(70)
-m.insert(72)
-m.insert(66)
-m.remove(65)
-print(m.root.right.value)
+m.insert(65)
+m.remove(50)
